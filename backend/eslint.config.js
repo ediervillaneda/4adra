4e9681473f53c56097e94445645680613d353b52
Ed Villan <ediervillaneda@gmail.com>
2026-07-14 01:22:46 -0500

    feat(backend): configurar ESLint y Prettier

---
+++ backend/eslint.config.js
@@ -0,0 +1,22 @@
+const tseslint = require('typescript-eslint');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = tseslint.config(
+  {
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
+  },
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+    },
+  },
+  eslintConfigPrettier,
+);
